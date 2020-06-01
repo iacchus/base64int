396b322ea411811ca01b121d96233c7c41445eab
Iacchus Mercurius <kassivs.@gmail.com>
2020-06-01 17:46:11 -0300

    readme, demo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Base64IP
+
+Transforms an IP address in a base64 string and vice versa. Execute the demo
+with:
+
+`python demo.py`
--- demo.py
+++ demo.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 from base64ip import Base64IP
 
-test = Base64IP(ip='192.168.0.1')
+a = Base64IP(ip='192.168.0.1')
 
+for attr, val in a.__dict__.items():
+    print(attr, '=', val)
 
