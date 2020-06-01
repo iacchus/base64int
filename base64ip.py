61ceb70465e6303a95f90ed0294acb9e3231247e
Iacchus Mercurius <kassivs.@gmail.com>
2020-06-01 17:56:54 -0300

    readme, demo

--- base64ip.py
+++ base64ip.py
@@ -10,10 +10,11 @@ class Base64IP:
 
 
     def __init__(self, ip=None, base64=None):
+
         if ip:
             self.ip_to_base64(ip)
         elif base64:
-            self.base64_to_ip
+            self.base64_to_ip(base64)
 
 
     def ip_to_base64(self, ip_str):
@@ -31,7 +32,8 @@ class Base64IP:
         self.byte_list = list(map(lambda x: int(x, base=self.BYTE_NOTATION_BASE),
                             ip_str.strip().split(self.BYTE_SEP)))
 
-        self.bitmap_list = [self._byte_to_bitmap(byte) for byte in self.byte_list]
+        self.bitmap_list = [self._byte_to_bitmap(byte)
+                            for byte in self.byte_list]
         self.ip_bitmap = "".join(self.bitmap_list)
         self.ip_decimal = int(self.ip_bitmap, base=2)
         self.b64_index_binary = [self.ip_bitmap[index:index+BASE64_INDEX_BITS]
@@ -40,16 +42,18 @@ class Base64IP:
         self.b64_index_integer = [int(bin_index, base=2)
                             for bin_index in self.b64_index_binary]
 
-        self.b64_ip = str().join([TABLE[index] for index in self.b64_index_integer])
-
         self.padding_size = len(self.ip_bitmap) % 3
         self.padding = self.padding_size * BASE64_PADDING_CHAR
+
+        self.b64_ip = str().join([TABLE[index] for index
+                                  in self.b64_index_integer])
+        self.b64_ip += self.padding
 
         return self.b64_ip + self.padding
 
 
-    def base64_to_ip(self, ip_b64str):
 
+    def base64_to_ip(self, ip_b64str):
 
         self.PADDING_COUNT = ip_b64str.count(BASE64_PADDING_CHAR)
         self.bitmap_list = list()
@@ -61,7 +65,7 @@ class Base64IP:
             self.bitmap_list += [bitmap]
 
         self.bitmap_list[-1] = self.bitmap_list[-1][2*self.PADDING_COUNT:]
-        self.ip_bitmap = "".join(bitmap_list)
+        self.ip_bitmap = "".join(self.bitmap_list)
 
         self.bytes_ip_binary = [self.ip_bitmap[index:index+BYTE_BITS]
                             for index in range(0, len(self.ip_bitmap), BYTE_BITS)]
